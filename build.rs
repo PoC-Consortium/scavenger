3ad3fccce0e6fc5ad3900cb412fdb58e41b310a7
Andy Prock <aprock@gmail.com>
2018-08-03 18:53:00 -0700

    enable mac os builds
    copy shabal64-darwin.s from engraver
    update build.rs to detect macos

--- build.rs
+++ build.rs
@@ -1,8 +1,19 @@
 extern crate cc;
 
 fn main() {
-    cc::Build::new()
-        .file("src/c/shabal64.s")
+    use std::env;
+
+    let target_os = env::var("CARGO_CFG_TARGET_OS");
+
+    let mut config = cc::Build::new();
+
+    if (target_os == Ok("macos".to_string())) {
+        config.file("src/c/shabal64-darwin.s");
+    } else {
+        config.file("src/c/shabal64.s");
+    }
+
+    config
         .file("src/c/mshabal_128.c")
         .file("src/c/mshabal_256.c")
         .file("src/c/shabal.c")
