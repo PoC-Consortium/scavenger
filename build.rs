3e41fa6e8c42353b056da2e74d578225b3490703
JohnnyFFM <JohnnyDeluxe@habmalnefrage.de>
2018-08-07 10:51:14 +0200

    msvc build flags

--- build.rs
+++ build.rs
@@ -3,13 +3,18 @@ extern crate cc;
 fn main() {
     let mut config = cc::Build::new();
 
+    #[cfg(target_env = "msvc")]
+    config.flag("/arch:AVX2");
+    #[cfg(not(target_env = "msvc"))]
+    config.flag("-mavx2").flag("-std=c99").flag("-march=native");
+
     config
         .file("src/c/sph_shabal.c")
         .file("src/c/mshabal_128.c")
         .file("src/c/mshabal_256.c")
         .file("src/c/shabal.c")
-        .flag("-mavx2")
-        .flag("-std=c99")
-        .flag("-march=native")
+        .file("src/c/mshabal_128.c")
+        .file("src/c/mshabal_256.c")
+        .file("src/c/shabal.c")
         .compile("libshabal.a");
 }
