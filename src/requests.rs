758840847b8213df111e2044426e63fb888c8a61
Bernhard Specht <bernhard@specht.net>
2018-08-09 01:04:48 +0800

    rust fmt

--- src/requests.rs
+++ src/requests.rs
@@ -219,8 +219,7 @@ impl RequestHandler {
             .and_then(|body| {
                 let res = parse_json_result(&body)?;
                 Ok(res)
-            })
-            .from_err();
+            }).from_err();
 
         let timeout = Timeout::new(self.timeout, &self.handle).unwrap();
         let timeout = timeout
