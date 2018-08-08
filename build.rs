b55f5c4fdd76d9e275234b5cdc6e4ee038368e48
JohnnyFFM <JohnnyDeluxe@habmalnefrage.de>
2018-08-08 14:07:55 +0200

    added msvc optimisation compiler flags

--- build.rs
+++ build.rs
@@ -4,7 +4,14 @@ fn main() {
     let mut config = cc::Build::new();
 
     #[cfg(target_env = "msvc")]
-    config.flag("/arch:AVX2");
+    config
+        .flag("/O2")
+        .flag("/Oi")
+        .flag("/Ot")
+        .flag("/Oy")
+        .flag("/GT")
+        .flag("/GL")
+        .flag("/arch:AVX2");
     #[cfg(not(target_env = "msvc"))]
     config.flag("-std=c99").flag("-march=native");
 
@@ -16,9 +23,7 @@ fn main() {
         #[cfg(not(target_env = "msvc"))]
         config.flag("-mavx2");
 
-        config
-            .file("src/c/mshabal_256.c")
-            .file("src/c/shabal.c");
+        config.file("src/c/mshabal_256.c").file("src/c/shabal.c");
     } else {
         config.file("src/c/shabal_sse2.c");
     }
