6867bd6a73b7838e767343be58391432a770a646
JohnnyFFM <JohnnyDeluxe@habmalnefrage.de>
2018-10-04 15:00:41 +0200

    android fix3

--- src/utils.rs
+++ src/utils.rs
@@ -54,10 +54,7 @@ cfg_if! {
                 .arg("-o")
                 .arg("PHY-SeC")
                 .output()
-                .unwrap_or_else(|| {
-                    warn!("failed to determine sector size, defaulting to 4096.");
-                    &"PHY-SEC\n   4096"
-                });
+                .expect("failed to execute 'lsblk -o PHY-SeC'");
 
             let sector_size = String::from_utf8(output.stdout).expect("not utf8");
             let sector_size = sector_size.split('\n').collect::<Vec<&str>>().get(1).unwrap_or_else(|| {
@@ -69,7 +66,9 @@ cfg_if! {
         }
 
         pub fn get_sector_size(path: &str) -> u64 {
-            if cfg!(target_os = "macos") {
+            if cfg!(target_arch = "aarch64") {
+                4096
+            } else if cfg!(target_os = "macos") {
                 get_sector_size_macos(path)
             } else {
                 get_sector_size_unix(path)
